1d422cf76d141cfd0def6db80d50313ddfbc914b
Ivan Skorokhod <ivanskorokhod@126.com>
2022-05-06 16:29:28 +0800

    Initial Commit

---
+++ Sveta's task by Ivan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sveta's task by Ivan
+//
+//  Created by mac on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
